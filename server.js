07f9a9d347d3ec3b781590f7e88ee4f75510e6f3
HoangVu2312 <103991547+HoangVu2312@users.noreply.github.com>
2023-05-11 22:56:03 +0300

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const { verifyToken } = require('./auth');
 // Set up socket-io
 const {Server} = require('socket.io');
 const io = new Server(server, {
-  cors: 'https://ecommerce.onrender.com',
+  cors: 'https://ecommerce-gsv7.onrender.com',
   methods: ['GET', 'POST', 'PATCH', "DELETE"]
 })
 
